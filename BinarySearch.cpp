2ae333153bff072516c0bf51d161b6de01c9dc35
Dia-Bansal <78194499+Dia-Bansal@users.noreply.github.com>
2022-05-09 12:45:50 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,7 +5,7 @@ int binarysearch(int arr[],int n){
     cin>>key;
     int start=0;
     int end=n-1;
-    int mid=(start+end)/2;
+    int mid=start+(end-start)/2;
     while(start<=end){
         if(arr[mid]==key){
             return mid;
@@ -16,7 +16,7 @@ int binarysearch(int arr[],int n){
         else{
             end=mid-1;
         }
-        mid=(start+end)/2;    
+         mid=start+(end-start)/2;   
         
     }
     return -1;
